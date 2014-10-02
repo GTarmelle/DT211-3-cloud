8e822a0aefe0ad33da50fd31c937c57f3069a256
Armelle Gaha <d14123111@mydit.ie>
2014-10-02 15:27:57 +0000

    complite problem4

--- euler/problem4.py
+++ euler/problem4.py
@@ -1,20 +1,29 @@
 print("Find the largest palindrome made from the product of two 3-digit numbers.")
-x = y = 20
+x = y = 999
 largestp = 0
-def ispalindrome(n):
+def ispalindrome(n): # use to examine if a giving number is a palindrome
   nToString = str(n)
   arglenght = len(nToString)
-  argReverseLenght = arglenght
-  for i in range(1,arglenght):
-    if(nToString[i] != 
-  print(len(strn))
-  
+  argReverseLenght = arglenght -1
+  for i in range(0,(arglenght -1)):
+    if(nToString[i] != nToString[argReverseLenght]):
+      return bool(0)
+    else:
+      argReverseLenght -= 1
+    
+  return bool(1)
+
+#go throught all 3 digit numbers o find the largest palindrome
+maxp = 0
 while(x>0):
   while(y>0):
     largestp = x*y
-    ispalindrome(largestp)
+    if(ispalindrome(largestp)):
+      if(maxp < largestp):
+        maxp = largestp
     y -=1
   x -=1
-    #spalindrome(largestp)
+  y = 999
+print(maxp)
     
     
